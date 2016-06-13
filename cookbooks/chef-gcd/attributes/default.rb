59933423805393815a83b86a749e2ec7a865b950
Priit Pääsukene <priit.paasukene@gmail.com>
2016-06-13 23:20:01 +0300

    wrapper for chef-ha package

--- cookbooks/chef-gcd/attributes/default.rb
+++ cookbooks/chef-gcd/attributes/default.rb
@@ -1,9 +1,13 @@
-default['chef-ha']['version'] = nil
-default['chef-ha']['package_source'] = nil
 
-# The Chef Server must have an API FQDN set.
-# Ref. http://docs.chef.io/install_server_pre.html#hostnames
-default['chef-ha']['api_fqdn'] = node['fqdn']
+default['chef_server_ha_aws_provisioner']['chef_server_url'] = 'https://localhost:4000'
+default['chef_server_ha_aws_provisioner']['chef_oauth2_app_id'] = '14dfcf186221781cff51eedd5ac1616'
+default['chef_server_ha_aws_provisioner']['chef_oauth2_secret'] = 'a49402219627cfa6318d58b13e90aca'
+default['chef_server_ha_aws_provisioner']['chef_oauth2_verify_ssl'] = false
 
-default['chef-ha']['topology'] = 'standalone'
-default['chef-ha']['addons'] = []
+
+
+#default['chef_server_ha_aws_provisioner']['package_version'] = '1.2.3'
+
+# install from the repository of nightly packages
+#default['chef_server_ha_aws_provisioner']['package_repo'] = ''
+default['chef_server_ha_aws_provisioner']['package_url'] = 'https://packages.chef.io/stable/ubuntu/14.04/chef-ha_1.0.0-1_amd64.deb'
--- cookbooks/chef-gcd/metadata.rb
+++ cookbooks/chef-gcd/metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures chef-gcd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
+depends 'chef-ingredient', '>= 0.18.0'
+depends 'chef-server-ha-aws-provisioner'
